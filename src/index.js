6e1e4801eda061d6fd8d5eb68dfe2a55142f772e
neymeka <megustafather@gmail.com>
2018-02-22 18:02:45 +0300

    master/feacher: Love Triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  preferences.unshift ([]);
+  var i, counter = 0
+  for (i = 1; i < preferences.length; i++) {
+    var love = preferences [i];
+    var love2 = preferences [love];
+    var love3 = preferences [love2]
+    if (i == love3) {
+      counter++;
+      }
+  }
+  return Math.floor (counter / 3);
+}
